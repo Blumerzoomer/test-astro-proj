b5fd948bf87f0ac7c77deaa3e0dac0585f4b60be
Blumerzoomer <oleksii.freelance.work@gmail.com>
2025-10-16 16:09:25 +0300

    add first version

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,106 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const links = [
+    {
+      icon: "/bg/facebook.svg",
+      link: "https://www.facebook.com/",
+    },
+    {
+      icon: "/bg/insta.svg",
+      link: "https://www.instagram.com/",
+    },
+    {
+      icon: "/bg/youtoobe.svg",
+      link: "https://www.youtube.com/",
+    },
+    {
+      icon: "/bg/x.svg",
+      link: "https://x.com/",
+    },
+  ];
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div
+      className="font-sans container flex w-full flex-col items-center justify-items-center min-h-screen min-w-screen "
+      style={{ backgroundImage: "url('/bg/gb-img.jpg')" }}
+    >
+      <main className="flex flex-col row-start-1 items-center ">
         <Image
-          className="dark:invert"
-          src="/next.svg"
+          className=""
+          src="/bg/logo.png"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={400}
+          height={400}
           priority
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <Image
+          className="ml-[35px] mt-[-170px]"
+          src="/bg/poseLotos.png"
+          alt="Next.js logo"
+          width={280}
+          height={450}
+          priority
+        />
+        <div className="ml-[35px] mt-[-300px] relative h-auto">
+          <Image
+            className="h-auto"
+            src="/bg/ellipse.svg"
+            alt="Next.js logo"
+            width={0}
+            height={0}
+            sizes="80vw"
+            style={{ width: "80vw" }}
+            priority
+          />
+          <div className="absolute z-1 top-1/2 left-1/2 mt-[100px] -translate-x-1/2 -translate-y-1/2 w-full p-3 flex flex-col justify-center gap-[30px]"
+          style={{ backgroundImage: "url('bg/ellipse.svg')" }}>
+            <h1 className="text-[#F5E7BC] font-montserrat text-center text-[32px] font-bold w-fit max-w-[400px] mx-auto leading-8">
+              Your Astro Journey Starts Here...
+            </h1>
+            <p className="text-[#F5E7BC] font-montserrat mx-auto text-[24px] ">
+              Join the Private Beta’s Waitlist.
+            </p>
+            <div className="w-full">
+              <div className="w-full max-w-[360px] h-[60px] flex items-center justify-between p-[6px] mx-auto border border-[#F5E7BC]/30 rounded-md focus-within:border-[#F5E7BC]">
+                <span>👉</span>
+                <input
+                  type="text"
+                  placeholder="Enter your Email"
+                  className="w-full mx-2 my-1 placeholder:text-[#F5E7BC]/20 text-white/70 outline-0 bg-transparent"
+                />
+                <button className="h-full px-[20px] text-[#F5E7BC] bg-black rounded-md border border-[#F5E7BC]/20 cursor-pointer hover:scale-105 transition-all duration-300 ease-out will-change-transform">
+                  Send
+                </button>
+              </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+              <p className="mt-[15px] text-center text-[#8C8C8C] font-bold text-[14px]">
+                We respect your privacy. No spam, ever.
+              </p>
+            </div>
+            <ul className="w-fit flex flex-row gap-6 mx-auto">
+              {links.map((item) => (
+                <li
+                  key={item.link}
+                  className="cursor-pointer hover:scale-110 transition-all duration-300 ease-out"
+                >
+                  <Link href={item.link}>
+                    <Image
+                      className="h-auto"
+                      src={item.icon}
+                      alt="icon logo"
+                      width={30}
+                      height={30}
+                      priority
+                    />
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
