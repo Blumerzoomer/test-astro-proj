1b8a60ed16b3f08560bc1a6d11f49692d9b9c1f5
Blumerzoomer <oleksii.freelance.work@gmail.com>
2025-10-17 14:50:33 +0300

    added changes

--- components/home/HomeSection.tsx
+++ components/home/HomeSection.tsx
@@ -1,14 +1,19 @@
 import HomeSendEmail from "./HomeSendEmail/HomeSendEmail";
 import HomeLinks from "./HomeLinks";
 import HomeImages from "./HomeImages/HomeImages";
+import classNames from "classnames";
 
 export default function HomeSection() {
   return (
     <div className="w-full">
-      <HomeImages/>
-      <div className="flex  flex-col max-w-[517px] mx-auto items-center w-full my-auto px-4 pb-4 absolute z-[3] top-[78vh] sm:top-[82vh] left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <HomeImages />
+      <div
+        className={classNames(
+          "flex flex-col max-w-[520px] mx-auto items-center w-full my-auto px-4 absolute z-[3] top-[78vh] sm:top-[81vh] 2xl:top-[calc(83vh-10px)] left-1/2 -translate-x-1/2 -translate-y-1/2"
+        )}
+      >
         <div className="w-full">
-          <h1 className="text-[var(--primary-color)] font-montserrat text-center text-[20px] mb-[14px] sm:mb-[29px] sm:text-[32px] font-bold w-fit max-w-[400px] mx-auto leading-8">
+          <h1 className="text-[var(--primary-color)] font-montserrat text-center text-[20px] mb-[14px] sm:mb-[29px] sm:text-[32px] font-bold w-fit max-w-[400px] mx-auto leading-6 sm:leading-8">
             Your Astro Journey <br /> Starts Here...
           </h1>
           <p className="text-[var(--primary-color)] font-montserrat mx-auto text-[16px] sm:text-[24px] text-center leading-5">
